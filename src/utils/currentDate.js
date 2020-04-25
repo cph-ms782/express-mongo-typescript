ded929cc8dde3822f9a19252c2a78f5a6cc82b75
Martin <cph-ms782@cphbusiness.dk>
2020-04-25 21:57:36 +0200

    changing timezone from UTC to frankfurt time

--- src/utils/currentDate.js
+++ src/utils/currentDate.js
@@ -1,30 +1,30 @@
 const currentDateTime = () => {
 	let currentdate = new Date();
-	let month = String(currentdate.getUTCMonth() + 1);
+	let month = String(currentdate.getMonth() + 1);
 	if (month.length == 1) {
 		month = '0' + month;
 	}
 
-	let day = String(currentdate.getUTCDate());
+	let day = String(currentdate.getDate());
 	if (day.length == 1) {
 		day = '0' + day;
 	}
 
-	let hours = String(currentdate.getUTCHours());
+	let hours = String(currentdate.getHours());
 	// console.log('hours', hours);
 	if (hours.length == 1) {
 		hours = '0' + hours;
 	}
 	// console.log('hours', hours);
 
-	let minutes = String(currentdate.getUTCMinutes());
+	let minutes = String(currentdate.getMinutes());
 	// console.log('minutes', minutes);
 	if (minutes.length == 1) {
 		minutes = '0' + minutes;
 	}
 	// console.log('minutes', minutes);
 
-	let seconds = String(currentdate.getUTCSeconds());
+	let seconds = String(currentdate.getSeconds());
 	// console.log('seconds', seconds);
 	if (seconds.length == 1) {
 		seconds = '0' + seconds;
@@ -33,7 +33,7 @@ const currentDateTime = () => {
 	// console.log('seconds', seconds);
 
 	const dt =
-		String(currentdate.getUTCFullYear()) +
+		String(currentdate.getFullYear()) +
 		'-' +
 		month +
 		'-' +
