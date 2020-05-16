3fa90c70bcbf7cab17c12ad00513aa43a082771c
Martin <cph-ms782@cphbusiness.dk>
2020-05-16 14:59:39 +0200

    build react folder to be deployed with nodejs

---
+++ public/precache-manifest.07850c2dbff5e211c850abd406ebc27e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d649704b525584ce18a6125969213f09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff29a5e5fc08ae406ae8",
+    "url": "/static/css/main.6777de90.chunk.css"
+  },
+  {
+    "revision": "1587a34889eb63538a80",
+    "url": "/static/js/2.85daeeb1.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.85daeeb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff29a5e5fc08ae406ae8",
+    "url": "/static/js/main.6e5027d5.chunk.js"
+  },
+  {
+    "revision": "e07e4fa9ba9e3dc62004",
+    "url": "/static/js/runtime-main.29a10bd9.js"
+  }
+]);
